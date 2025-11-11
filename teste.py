cd8c0726631e5249f05c6522c2a1634648e2a8cf
vivi857 <62722390+vivi857@users.noreply.github.com>
2025-11-10 21:50:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,1 @@
-import fastapi, requests, dotenv
-print("Ambiente configurado e funcionando 🎉")
 
-
-import sqlite3
-conn = sqlite3.connect("pokemon.db")
-c = conn.cursor()
-c.execute("SELECT COUNT(*) FROM pokemon")
-print("Total de pokémons:", c.fetchone()[0])
-conn.close()
